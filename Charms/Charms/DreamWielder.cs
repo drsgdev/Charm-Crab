0c69064b6c9e5892e4579c1af60093433e8fa247
DistractionCrab <6255349+DistractionCrab@users.noreply.github.com>
2022-09-28 16:40:45 -0400

    Buffed Dream Wielder

--- Charms/Charms/DreamWielder.cs
+++ Charms/Charms/DreamWielder.cs
@@ -5,6 +5,8 @@ using Vasi;
 
 namespace CharmCrab.Charms {
 	class DreamWielder {
+		public static readonly float MULT = 1.5f;
+
 		// Cooldown being used a duration tool. Check for !Available to see if it's active.
 		private Cooldown cd = new Cooldown(5f);
 
@@ -18,7 +20,7 @@ namespace CharmCrab.Charms {
 
 		public int Damage(int i) {
 			if (CharmData.Equipped(Charm.DreamWielder) && !this.cd.Available) {
-				return (int)(i * 1.2);
+				return (int)(i * MULT);
 			} else {
 				return i;
 			}
