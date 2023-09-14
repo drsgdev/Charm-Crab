c2c37c772572f39792fb019e543ce46669fe588b
Alexey Babushkin <alexbabushkin14@hotmail.com>
2023-09-14 23:46:09 +0000

    apply stack on hit

--- Charms/Charms/MarkOfPride.cs
+++ Charms/Charms/MarkOfPride.cs
@@ -10,38 +10,40 @@ namespace CharmCrab.Charms {
 		private readonly float ResetTime = 30;
 		private int stacks = 0;
 		private float timeSinceLast = 0;
+
+		public int Stacks {
+			get { return stacks; }
+		}
+
 		public void Start() { }
 
 		public void Update() {
 			timeSinceLast += Time.deltaTime;
-			if (this.timeSinceLast > ResetTime) {
-				this.stacks = 0;
+			if (timeSinceLast > ResetTime) {
+				stacks = 0;
 			}
 		}
 
-		public int Stacks {
-			get { return this.stacks; }
+		private void ApplyStack() {
+			stacks =+ 1;
+			timeSinceLast = 0;
 		}
 
 		public int DamageBonus(int i) {
 				if (PlayerData.instance.GetBool("equippedCharm_13")) {
-					return i + DamageScale * this.stacks * (2 + PlayerData.instance.GetInt("nailSmithUpgrades"));
+					int damageBonus = i + DamageScale * stacks * (2 + PlayerData.instance.GetInt("nailSmithUpgrades"));
+					ApplyStack();
+
+					return damageBonus;
 				}
 				return i;
 		}
 
 		public void TakeDamage(int i) {
 			if (i > 0) {
-				this.stacks = 0;
-				this.timeSinceLast = 0;
+				stacks = 0;
+				timeSinceLast = 0;
 			}
 		}
-
-		public void ApplyStack() {
-			stacks =+ 1;
-			timeSinceLast = 0;
-		}
-
-
 	}
 }
